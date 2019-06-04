1759b61a1d9de84b84161d7652416c7514410f0b
01 <ahmedmagdy92@yahoo.com>
2019-06-04 10:46:24 +0200

    changing back to sqlite

--- shorten/settings.py
+++ shorten/settings.py
@@ -64,11 +64,8 @@ WSGI_APPLICATION = 'shorten.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'urlshortener',
-        'HOST': 'db4free.net',
-        'USER': 'urlshortener',
-        'PASSWORD':'fakegame',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
